30e47a198eafe09a95f4d4d50a907a0733478521
45sangmin99 <45sangmin99@users.noreply.github.com>
2026-09-11 16:30:10 +0900

    Q3_A_LL.c 풀이 완료

--- Linked_List/Q3_A_LL.c
+++ Linked_List/Q3_A_LL.c
@@ -87,6 +87,20 @@ int main()
 void moveOddItemsToBack(LinkedList *ll)
 {
 	/* add your code here */
+	ListNode *cur = ll->head;
+	int temp = ll->size;
+	for(int i = 0; i < temp; i++){
+		if(cur->item%2 == 1){
+			insertNode(ll, ll->size, cur->item);
+			cur = cur->next;
+			removeNode(ll, i);
+			temp --;
+			i--;
+		}else{
+			cur = cur->next;
+		}
+	}
+	return;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
